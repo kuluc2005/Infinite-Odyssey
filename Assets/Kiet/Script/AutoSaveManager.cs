2282bfcf391a0b902699e678c730339ede137b99
Conan2208 <kietnguyen22085@gmail.com>
2025-05-28 16:05:13 +0700

    UI_Menu

---
+++ Assets/Kiet/Script/AutoSaveManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AutoSaveManager : MonoBehaviour
+{
+    void OnApplicationQuit()
+    {
+        SaveCurrentScene();
+    }
+
+    void SaveCurrentScene()
+    {
+        string currentScene = SceneManager.GetActiveScene().name;
+        PlayerPrefs.SetString("SaveScene", currentScene);
+        PlayerPrefs.Save();
+        Debug.Log("Đã tự động lưu scene: " + currentScene);
+    }
+}
