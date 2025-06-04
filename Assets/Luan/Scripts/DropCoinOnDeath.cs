38d3aa9832e34a056fcb8562991da50c9ab2801c
Vampire <nguyenluan01042005@gmail.com>
2025-06-04 12:43:57 +0700

    Update Enemy

---
+++ Assets/Luan/Scripts/DropCoinOnDeath.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DropCoinOnDeath : MonoBehaviour
+{
+    public GameObject coinPrefab; // Prefab đã setup như bạn có
+    public Vector3 spawnOffset = new Vector3(0, 0.1f, 0); // Cho coin hơi lơ lửng
+
+    public void DropCoin()
+    {
+        Instantiate(coinPrefab, transform.position + spawnOffset, Quaternion.identity);
+    }
+}
