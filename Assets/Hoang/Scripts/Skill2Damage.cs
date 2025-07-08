4b7581c2e2737bcd2ed9f0cc38ecf0997911d9b2
Fove293 <tranhuuhoang2021@gmail.com>
2025-07-08 15:11:05 +0700

    Skill2Butterfly

---
+++ Assets/Hoang/Scripts/Skill2Damage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Skill2Damage : MonoBehaviour
+{
+    public GameObject hitVFXPrefab;
+    public float skillDuration = 3f;
+
+    void Start()
+    {
+        Destroy(gameObject, skillDuration); // skill tự huỷ sau X giây
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            // Hiệu ứng trúng đòn (liên tục mỗi frame)
+            Instantiate(hitVFXPrefab, other.transform.position, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Hoang/Scripts/SkillController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class SkillController : MonoBehaviour
+{
+    public Animator animator;
+
+    [Header("Skill2 Settings")]
+    public GameObject skill2AuraPrefab;
+    public Transform auraSpawnPoint;
+
+    public GameObject skill2VFXPrefab;
+    public Transform skill2SpawnPoint;
+
+    public float skill2Delay = 1f; // thời gian chờ trước khi skill xuất hiện
+
+
+
+    
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            UseSkill2();
+        }
+    }
+
+    void UseSkill2()
+    {
+        animator.SetTrigger("Skill2");
+
+        // Spawn aura quanh người
+        if (skill2AuraPrefab && auraSpawnPoint)
+        {
+            GameObject aura = Instantiate(skill2AuraPrefab, auraSpawnPoint.position, auraSpawnPoint.rotation, transform);
+            Destroy(aura, 2f); // tự huỷ sau 2s
+        }
+
+        // Sau 1s thì mới spawn skill
+        Invoke(nameof(SpawnSkill2VFX), skill2Delay);
+    }
+
+    void SpawnSkill2VFX()
+    {
+        if (skill2VFXPrefab && skill2SpawnPoint)
+        {
+            Instantiate(skill2VFXPrefab, skill2SpawnPoint.position, skill2SpawnPoint.rotation);
+        }
+    }
+}
