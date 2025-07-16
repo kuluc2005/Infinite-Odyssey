672ecf819510a5f0a99cdb2281eb9a8bd05e5505
kuluc2005 <lyminhluc6@gmail.com>
2025-07-16 12:49:20 +0700

    Audio

--- Assets/Luc/Scripts/BackgroundVideo.cs
+++ Assets/Luc/Scripts/BackgroundVideo.cs
@@ -4,26 +4,38 @@ using UnityEngine.Video;
 public class BackgroundVideo : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
+    public string videoFileName = "BackGVideo1.mp4"; 
+    public RenderTexture sharedRenderTexture;
+
+    public AudioSource bgMusicSource;
+    public AudioClip bgMusicClip;
 
     void Start()
     {
-        string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, "BackGVideo1.mp4");
+        if (bgMusicSource && bgMusicClip)
+        {
+            bgMusicSource.clip = bgMusicClip;
+            bgMusicSource.loop = true;
+            bgMusicSource.Play();
+        }
+
+        string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, videoFileName);
 
 #if UNITY_EDITOR || UNITY_STANDALONE_WIN
         videoPlayer.url = "file:///" + videoPath;
 #else
-    videoPlayer.url = videoPath;
+        videoPlayer.url = videoPath;
 #endif
 
         if (System.IO.File.Exists(videoPath))
         {
+            videoPlayer.targetTexture = sharedRenderTexture; 
             videoPlayer.isLooping = true;
             videoPlayer.Play();
         }
         else
         {
-            Debug.LogError("Video file not found at: " + videoPath);
+            Debug.LogError("Video not found: " + videoPath);
         }
     }
-
 }
